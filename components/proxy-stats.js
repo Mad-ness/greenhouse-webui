e7bc0cbf46df21b1c690043697f261b86a457d67
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-06-27 10:38:21 +0300

    `Easily updated the component code

--- components/proxy-stats.js
+++ components/proxy-stats.js
@@ -10,21 +10,20 @@ export default {
     mounted: function() {
         setInterval(function() {
             this.loading = true;
+            function pad(num, size) {
+                var s = "" + num;
+                while ( s.length < size )
+                    s = "0" + num;
+                return s;
+            }
             axios
                 .get('/proxy/uptime')
                 .then(response => {
-                    Number.prototype.pad = function(size) {
-                        var s = String(this);
-                        while (s.length < ( size || 2 ))
-                            s = "0" + s;
-                        return s;
-                    }
                     var uptime = response.data.data.uptime;
-
                     var hrs = Math.trunc(uptime/(60*60));
                     var mins = Math.trunc((uptime - hrs*60*60)/60);
                     var secs = uptime - hrs*60*60 - mins*60;
-                    this.value = hrs.pad(2) + ':' + mins.pad(2) + ':' + mins.pad(2);
+                    this.value = pad(hrs,2) + ':' + pad(mins, 2) + ':' + pad(secs,2);
                     this.connected = true;
                 })
                 .catch(error => {
@@ -33,7 +32,7 @@ export default {
                 })
                 .finally(() => this.loading = false);
                 this.loading = false;
-        }.bind(this), 1500)},
+        }.bind(this), 5000)},
     template: `
         <div>
             Proxy uptime:
